614a8ce556d5ea89007581684f64b7490fa6cb3e
Mohammed Assiri <q2me@live.com>
2023-04-10 20:45:51 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'homepage/home'
+  root 'homepage#home'
   get 'homepage/about'
-  get 'homepage/contact'
+  get 'about', to: 'homepage#about'
+  get 'contact', to: 'homepage#contact'
+  get 'w', to: 'homepage#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
